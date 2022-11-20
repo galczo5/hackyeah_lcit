7af0bb7f8e93c3882ce7ae90301eb4033704fec7
Illia Mishkin <illia.mishkin@softwareplant.com>
2022-11-20 06:34:12 +0100

    Create page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'new-scene',
+    loadChildren: () => import('./scene/SceneModule').then(m => m.SceneModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
